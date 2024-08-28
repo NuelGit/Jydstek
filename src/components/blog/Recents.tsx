7782d3d866ecfcc85cc5d8fab326ecd1d8c7c16a
NuelGit <zioneemma@gmail.com>
2024-08-28 18:39:09 +0100

    updated jydstek website

--- src/components/blog/Recents.tsx
+++ src/components/blog/Recents.tsx
@@ -7,21 +7,21 @@ const Recents: React.FC = () => {
         <h5>Recent Works</h5>
         <img src="/assets/img/blog/recent-post-bg.jpg" alt="" />
         <div className="single-recent-post">
-          <h6>Hotels !</h6>
+          <h6>Innovation & Technology!</h6>
           <p className="blog-date">
             <i className="las la-calendar"></i>April 10, 2023
           </p>
         </div>
         <img src="/assets/img/blog/recent-post-bg-2.jpg" alt="" />
         <div className="single-recent-post">
-          <h6>Seven ways to support legal procedure</h6>
+          <h6>Design & Aesthetics</h6>
           <p className="blog-date">
             <i className="las la-calendar"></i>March 30, 2023
           </p>
         </div>
         <img src="/assets/img/blog/recent-post-bg-3.jpg" alt="" />
         <div className="single-recent-post">
-          <h6>How to Help Your suprior attorney</h6>
+          <h6>Safety & Reliability</h6>
           <p className="blog-date">
             <i className="las la-calendar"></i>February 18, 2023
           </p>
